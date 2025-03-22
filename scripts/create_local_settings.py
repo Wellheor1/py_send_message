45f2b4b55972b764e3b4601510dc25ccc75ee42b
well <Well.heor@yandex.ru>
2025-03-22 15:43:35 +0800

    Скрипт для копирования настроек

---
+++ scripts/create_local_settings.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+root_dir = os.path.dirname(current_dir)
+settings_dir = os.path.join(root_dir, 'settings')
+local_settings_dir = os.path.join(root_dir, 'settings', 'local_settings')
+ignore_files = {'__init__.py', '__pycache__', 'local_settings'}
+print(f'Копирование файлов\nиз: {settings_dir}\nв {local_settings_dir}')
+
+settings_files = set(os.listdir(settings_dir)) - ignore_files
+local_settings_files = set(os.listdir(local_settings_dir)) - ignore_files
+
+local_settings_files = {file_name.split('_')[1] for file_name in local_settings_files}
+
+files_to_copy = settings_files - local_settings_files
+print('Следующие файлы будут скопированы:', files_to_copy)
+for file_name in files_to_copy:
+    shutil.copy(os.path.join(settings_dir, file_name), f"{local_settings_dir}/local_{file_name}")
+print('Файлы успешно скопированы')
