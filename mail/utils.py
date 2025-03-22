64ed866b0face7111f75ec740f69cda8fae918f1
well <Well.heor@yandex.ru>
2025-03-22 16:39:51 +0800

    fixs

--- mail/utils.py
+++ mail/utils.py
@@ -4,8 +4,7 @@ from fastapi import HTTPException
 
 async def check_settings():
     mail_dir = os.path.dirname(os.path.abspath(__file__))
-    print(mail_dir)
-    local_settings_path = os.path.join("/local_settings.py")
+    local_settings_path = os.path.join(mail_dir, "/local_settings.py")
     module_configured = os.path.exists(local_settings_path)
     if not module_configured:
         raise HTTPException(status_code=500, detail="module mail is not configured")
