a4b6f873946afc7892ac7a2bfab13897c0e368d7
well <Well.heor@yandex.ru>
2025-03-23 19:20:49 +0800

    тип для вложений

--- mail/types.py
+++ mail/types.py
@@ -1,11 +1,18 @@
 from pydantic import BaseModel
 
 
+class Attachment(BaseModel):
+    encoding: str
+    filename: str
+    content: str
+    contentType: str
+
+
 class Email(BaseModel):
     to: str | list
     subject: str
-    message: str
-    attachments: str | list
+    message: str | None
+    attachments: list[Attachment] | None
 
 
 class Emails(BaseModel):
