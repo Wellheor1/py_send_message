1697d725e1894a8c557f6ef2837f809b6e3fd0a9
well <Well.heor@yandex.ru>
2025-03-23 16:15:08 +0800

    fix

--- mail/utils.py
+++ mail/utils.py
@@ -1,7 +1,7 @@
 import logging
 import os
 
-logger = logging.getLogger("mail")
+logger = logging.getLogger("uvicorn.error")
 
 
 def check_settings():
@@ -9,4 +9,4 @@ def check_settings():
     local_settings_path = os.path.join(mail_dir, "local_settings.py")
     local_settings_exists = os.path.exists(local_settings_path)
     if not local_settings_exists:
-        logger.error("The mail module is enabled, but there is no local settings file.")
+        logger.warning("The mail module is enabled, but there is no local settings file.")
