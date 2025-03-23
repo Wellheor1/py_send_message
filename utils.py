50199daa5b607122886563a3e352ade45998852e
well <Well.heor@yandex.ru>
2025-03-23 16:12:27 +0800

    fix

--- utils.py
+++ utils.py
@@ -9,5 +9,4 @@ def check_settings():
     local_settings_path = os.path.join(project_dir, "local_settings.py")
     local_settings_exists = os.path.exists(local_settings_path)
     if not local_settings_exists:
-        logger.error("fdfsd")
-        logger.error("There is no local settings file for the main module")
+        logger.warning("There is no local settings file for the main module")
