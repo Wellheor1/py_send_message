931d0d1a824d9e3b64fe0ed033ebd75448cc9861
well <Well.heor@yandex.ru>
2025-03-23 17:27:52 +0800

    fix

--- mail/main.py
+++ mail/main.py
@@ -4,12 +4,12 @@ from mail.settings import SMTP_HOST, SMTP_PORT, SMTP_USER, SMTP_PASSWORD, SMTP_S
 
 
 def send(emails: Emails):
-    smtp = smtplib.SMTP(SMTP_HOST, SMTP_PORT)
-    if SMTP_SECURE:
-        smtp.starttls()
-    smtp.login(SMTP_USER, SMTP_PASSWORD)
+    # smtp = smtplib.SMTP(SMTP_HOST, SMTP_PORT)
+    # if SMTP_SECURE:
+    #     smtp.starttls()
+    # smtp.login(SMTP_USER, SMTP_PASSWORD)
     for email in emails:
         print(email)
         # smtp.sendmail(SMTP_USER, email.to, mail_body)
-    smtp.quit()
+    # smtp.quit()
     return {"ok": True, "message": ""}
