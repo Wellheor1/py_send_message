093a58b4cbca823df6043bdf31a6fb6be5888adf
well <Well.heor@yandex.ru>
2025-04-16 17:03:13 +0800

    fix

--- app/mail/schemas.py
+++ app/mail/schemas.py
@@ -1,11 +1,6 @@
 from pydantic import BaseModel
 
 
-class SendResult(BaseModel):
-    ok: bool
-    errors: dict
-
-
 class Attachment(BaseModel):
     encoding: str | None = None
     filename: str
@@ -13,16 +8,8 @@ class Attachment(BaseModel):
     content_type: str
 
 
-class Attachments(BaseModel):
-    attachments: list[Attachment]
-
-
 class Email(BaseModel):
     to: str | list
     subject: str
     message: str | None = None
-    attachments: Attachments | None = None
-
-
-class Emails(BaseModel):
-    emails: list[Email]
+    attachments: list[Attachment] | None = None
--- app/router/mail.py
+++ app/router/mail.py
@@ -2,7 +2,7 @@ from fastapi.responses import JSONResponse
 from fastapi import APIRouter
 
 from app.mail.main import sends
-from app.mail.schemas import Emails
+from app.mail.schemas import Email
 from app.router.schemas import Result
 
 router = APIRouter(
@@ -12,6 +12,6 @@ router = APIRouter(
 
 
 @router.post("/sends", summary="Send email", response_model=Result)
-async def send_mails(body: Emails):
-    result = await sends(body.emails)
+async def send_mails(body: list[Email]):
+    result = await sends(body)
     return JSONResponse(result)
