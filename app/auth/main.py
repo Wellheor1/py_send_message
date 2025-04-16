2da8bd6eca79cf402c0870fd065050a36e64a03f
well <Well.heor@yandex.ru>
2025-04-16 17:54:35 +0800

    новая проверка bearer token

--- app/auth/main.py
+++ app/auth/main.py
@@ -1,10 +1,19 @@
-from fastapi import Header, HTTPException
+from fastapi import Header, HTTPException, Depends
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from typing_extensions import Annotated
 from app.settings import BEARER_TOKEN
 
+security = HTTPBearer()
+
 
 async def verify_bearer_token(authorization: Annotated[str, Header()]):
-    print(authorization)
     bearer_token = f"Bearer {BEARER_TOKEN}"
     if authorization != bearer_token:
+        raise HTTPException(status_code=401, detail="bearer token invalid")
+
+
+async def verify_bearer_token_v2(
+    credentials: HTTPAuthorizationCredentials = Depends(security),
+):
+    if credentials.credentials != BEARER_TOKEN:
         raise HTTPException(status_code=401, detail="bearer token invalid")
--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI, Depends
 from fastapi.responses import JSONResponse
-from app.auth.main import verify_bearer_token
+
+from app.auth.main import verify_bearer_token_v2
 from app.router.mail import router as mail_router
 from app.router.slog import router as slog_router
 from app.settings import MODULES
 from app.mail.utils import check_settings as mail_check_settings
 from app.utils import check_settings as main_check_settings
 
-app = FastAPI(dependencies=[Depends(verify_bearer_token)])
+app = FastAPI(dependencies=[Depends(verify_bearer_token_v2)])
 
 main_check_settings()
 
