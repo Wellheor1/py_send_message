ca549873be39a5383b4a766f6ed5adf1c91c63d9
well <Well.heor@yandex.ru>
2025-04-16 17:05:36 +0800

    fix

--- app/mail/main.py
+++ app/mail/main.py
@@ -20,6 +20,8 @@ async def sends(emails: list[Email]):
                     )
                 )
             result_send = server.sendmail(SMTP_USER, email.to, body.as_string())
+            # todo Продумать структуру ответа, возвращаются только ошибки в виде { 'some@mail.ru', (550,
+            #  'User Unknown') }
             if result_send:
                 result.append({"ok": False, "errors": result_send})
             else:
