4d2db37d0e1fe38343309eef6d0ef2ed7c1efc40
well <Well.heor@yandex.ru>
2025-04-17 22:06:47 +0800

    Общая функция для получения записей по id v2

--- app/dao/base.py
+++ app/dao/base.py
@@ -9,7 +9,7 @@ async def find_all(model, **filter_by):
         return result.scalars().all()
 
 
-async def find_one(model, entity_id: int):
+async def find_by_id(model, entity_id: int):
     async with async_session_maker() as session:
         query = select(model).filter_by(id=entity_id)
         result = await session.execute(query)
--- app/router/slog.py
+++ app/router/slog.py
@@ -1,7 +1,7 @@
 from fastapi.responses import JSONResponse
-from fastapi import APIRouter, Query
+from fastapi import APIRouter, Query, Path
 
-from app.dao.base import find_all
+from app.dao.base import find_all, find_by_id
 from app.slog.models import Slog
 from app.slog.query_params import QueryParamSlog
 from app.slog.schemas import SSlog
@@ -12,7 +12,13 @@ router = APIRouter(
 )
 
 
-@router.get("/", summary="get all logs", response_model=list[SSlog])
+@router.get("/", summary="Получить все записи логов", response_model=list[SSlog])
 async def get_all(query_params: QueryParamSlog = Query()):
     result = await find_all(Slog, **query_params.to_dict())
+    return JSONResponse(result)
+
+
+@router.get("/{slog_id}", summary="Получить логи по id", response_model=SSlog)
+async def get_by_id(slog_id: int = Path()):
+    result = await find_by_id(Slog, slog_id)
     return JSONResponse(result)
