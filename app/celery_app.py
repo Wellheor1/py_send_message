3c3d4a07f873f78e4e72d4d6aa274f249c0b2368
well <Well.heor@yandex.ru>
2025-06-12 23:34:11 +0800

    При ошибки celery будет выводится в лог

--- app/celery_app.py
+++ app/celery_app.py
@@ -26,13 +26,27 @@ celery_worker_process = None
 
 def start_worker():
     global celery_worker_process
-    celery_worker_process = subprocess.Popen(
-        ["celery", "-A", "app.celery_app", "worker", "--loglevel=info"],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        env=os.environ.copy(),
-    )
-    logger.info("Worker celery started")
+    try:
+        celery_worker_process = subprocess.Popen(
+            ["celery", "-A", "app.celery_app", "worker", "--loglevel=info"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            env=os.environ.copy(),
+            text=True,
+        )
+        stdout, stderr = celery_worker_process.communicate()
+        if celery_worker_process.returncode != 0:
+            logger.error(stderr)
+        else:
+            logger.info("Worker celery started")
+    except subprocess.CalledProcessError as e:
+        logger.error(f"Error when launching celery: {e}")
+    except FileNotFoundError:
+        logger.error(
+            "The 'celery' command was not found. Make sure that celery is installed."
+        )
+    except Exception as e:
+        logger.error(f"An unexpected error has occurred: {e}")
 
 
 def stop_worker():
