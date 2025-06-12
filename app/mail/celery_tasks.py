b496e27fab0ea86d373eecc786adf83e38639a21
well <Well.heor@yandex.ru>
2025-06-13 00:20:56 +0800

    send via celery

--- app/mail/celery_tasks.py
+++ app/mail/celery_tasks.py
@@ -1,8 +1,8 @@
 from app.celery_app import app as celery_app
+from app.mail.api import send
 
 
 @celery_app.task
-def send_email(recipient, subject, body):
-    # Логика отправки email
-    print(f"Отправка email: {subject} to {recipient}")
-    return f"Email sent to {recipient}"
+def send_email(email):
+    result_send = send(email)
+    return result_send
