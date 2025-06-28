2a6eab543a90819feb64f6f614ab3d5b101b95eb
well <Well.heor@yandex.ru>
2025-06-28 16:47:45 +0800

    fix

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,17 +1,26 @@
 import pytest
 from fastapi.testclient import TestClient
 from app.main import app
+from app.settings import BEARER_TOKEN
 
 client = TestClient(app)
 
 
 @pytest.mark.asyncio
-async def test_send_message():
+async def test_non_token():
     response = client.get("/")
+    assert response.status_code == 403
+
+
+@pytest.mark.asyncio
+async def test_invalid_token():
+    headers = {"authorization": "Bearer 434324"}
+    response = client.get("/", headers=headers)
+    assert response.status_code == 401
+
+
+@pytest.mark.asyncio
+async def test_valid_token():
+    headers = {"authorization": f"Bearer {BEARER_TOKEN}"}
+    response = client.get("/", headers=headers)
     assert response.status_code == 200
-
-
-# @pytest.mark.asyncio
-# async def test_send_message_empty():
-#     response = client.get("/3rkjjk")
-#     assert response.status_code == 422  # Ожидаем ошибку валидации
